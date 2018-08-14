5e67494e2caf608ad1fcd6eea0a61be00439fcb9
Gur Raunaq Singh <raunaq.soni+gurraunaqsingh@gmail.com>
2018-08-14 18:29:51 +0530

    First Blood

---
+++ app/src/main/java/easypaste/example/com/easypaste/MainActivity.java
@@ -0,0 +1,29 @@
+package easypaste.example.com.easypaste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        e1 = findViewById(R.id.editText);
+
+
+    }
+
+    public void send(View v){
+
+        MessageSender messageSender = new MessageSender();
+        messageSender.execute(e1.getText().toString());
+
+    }
+
+}
