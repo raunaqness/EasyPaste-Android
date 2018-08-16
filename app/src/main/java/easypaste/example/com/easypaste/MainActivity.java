abd41dbc650e42d50134a988d2f1806b0f8f5751
Gur Raunaq Singh <raunaq.soni+gurraunaqsingh@gmail.com>
2018-08-16 15:27:18 +0530

    QR Code Scanner added and Working

--- app/src/main/java/easypaste/example/com/easypaste/MainActivity.java
+++ app/src/main/java/easypaste/example/com/easypaste/MainActivity.java
@@ -1,14 +1,19 @@
 package easypaste.example.com.easypaste;
 
+
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText e1;
+
+    Button btnTakePicture, btnScanBarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,32 @@ public class MainActivity extends AppCompatActivity {
 
         e1 = findViewById(R.id.editText);
 
+        initViews();
 
     }
+
+    private void initViews() {
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnTakePicture.setOnClickListener(this);
+        btnScanBarcode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnTakePicture:
+                startActivity(new Intent(MainActivity.this, PictureBarcodeActivity.class));
+                break;
+            case R.id.btnScanBarcode:
+                startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
+                break;
+        }
+
+    }
+
+
 
     public void send(View v){
 
